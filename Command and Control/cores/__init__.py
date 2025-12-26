f9577f1bddac6ee67490dd4e75cb33f9ce0fb864
Francis Mike John Camogao <153460484+mikeedudee@users.noreply.github.com>
2025-12-26 23:58:14 +0800

    Delete Command and Control directory

--- Command and Control/cores/__init__.py
+++
@@ -1,3 +0,0 @@
-from .prompt_boxes      import Dialouge_Boxes
-from .clickable_label   import ClickableLabel
-from .bottom_toolbar    import BottomToolbar
--- Command and Control/cores/status_store.py
+++
@@ -1,7 +0,0 @@
-class SystemStatusStore:
-    def __init__(self, n_sensors=6, n_servos=7):
-        self.sensor_states = [0] * n_sensors
-        self.servo_states = [0] * n_servos
-
-# Create a single instance for global use
-status_store = SystemStatusStore()
